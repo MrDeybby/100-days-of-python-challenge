cc664c7aebdaa2b2be5e32c70a15ef22c2c2ceba
MrDeybby <deybbyrosario@gmail.com>
2026-01-01 23:19:09 -0400

    [Add] Day 2 and day log

---
+++ day1/hello_world.py
@@ -0,0 +1,12 @@
+# Welcome Message Generator
+# Step 1: Ask for user details
+# Step 2: Generate a personalized welcome message
+
+# Ask for name and favorite hobby
+# # Generate a personalized welcome message
+
+name = input("What's yor name: ")
+hobby = input("What's yor favorite hobby: ")
+
+print(f"Hi! {name}. Your favorite hobby is {hobby}")
+print("Hello World")
