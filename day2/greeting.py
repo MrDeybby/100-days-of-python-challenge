b9358137e30e74f5a0f9fbce5794ceeeea2033a8
MrDeybby <deybbyrosario@gmail.com>
2026-01-03 12:33:19 -0400

    [add] day 2 - greeting message

---
+++ day2/greeting.py
@@ -0,0 +1,11 @@
+# Greeting Program
+# Ask for user name, age, and favorite color
+# Generate a personalized greeting
+
+name = input("What's your name?: ")
+age = int(input("How old are you?: "))
+favorite_color = input("What's your favorite color?: ")
+
+# --- Greeting ---
+print(f"Hi {name}\nYou are {age} years old and your favorite color is {favorite_color}")
+
