e0338759e92a28235b07bf8959d2af4a89658083
MrDeybby <deybbyrosario@gmail.com>
2026-01-04 10:25:40 -0400

    [add] day 3

---
+++ day3/calculator.py
@@ -0,0 +1,12 @@
+# Ask for user input for two numbers
+# Add, Subtract, multiply and divide the numbers
+
+
+number1 = int(input("First number: "))
+number2 = int(input("Second number: "))
+
+print("Addition:", number1 + number2)
+print("Subtraction:", number1 / number2)
+print("Multiplication:", number1 / number2)
+if number2 != 0:
+    print("Division:", number1 / number2)
