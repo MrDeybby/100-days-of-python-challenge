98e9510ed84f48f84e874c6169f912f70630088c
MrDeybby <deybbyrosario@gmail.com>
2026-01-06 22:14:33 -0400

    [fix] correct subtraction and multiplication operations in calculator

--- day3/calculator.py
+++ day3/calculator.py
@@ -6,7 +6,7 @@ number1 = int(input("First number: "))
 number2 = int(input("Second number: "))
 
 print("Addition:", number1 + number2)
-print("Subtraction:", number1 / number2)
-print("Multiplication:", number1 / number2)
+print("Subtraction:", number1 - number2)
+print("Multiplication:", number1 * number2)
 if number2 != 0:
     print("Division:", number1 / number2)
